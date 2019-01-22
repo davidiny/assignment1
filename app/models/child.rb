874de9a2b84d4bfd67a8b4b33d38f4d2a5d0ed5b
David Inyangson <much2money@Davids-MacBook-Pro.local>
2019-01-22 17:29:37 -0500

    Finished part 3

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,12 @@
 class Child < ApplicationRecord
+    has_many :chores
+    has_many :tasks, through: :chores
+    validates_presence_of :first_name
+    validates_presence_of :last_name
     
+    scope :alphabetical, -> { Child.order(first_name: :asc) }
+    scope :active, -> { where(active: true) }
+    def name
+        return first_name + " " + last_name
+    end
 end
